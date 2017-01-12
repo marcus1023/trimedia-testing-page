2eedc93ec978b1d35b28f010fa141956f06ee008
Marcus Ogden <marcus@userlite.com>
2017-01-12 16:50:19 -0700

    testing env. created

---
+++ img/Trimobile_files/main.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+//
+
+
+
+$( "#test" ).click(function() {
+  $('.side-menu').toggleClass( "shower");
+  $('.main-header-container').toggleClass("show-me-200");
+  $('.burger-li').toggleClass("orange-background");
+  $('.scroller-nav').toggleClass("hide-me");
+});
+
+
+$("#test").hover(function(){
+    $('.burger-li').css("background-color", "#e37401");
+    }, function(){
+    $('.burger-li').css("background-color", "white");
+});
+
+$( ".digitales-options-one" ).click(function() {
+  $('.digitales-catigories-two').toggleClass( "shower");
+  $('.digitales-catigories-one').toggleClass( "hider");
+  $('.digitales-options-one').toggleClass( "digitales-b-border");
+  $('.digitales-options-two').toggleClass( "digitales-b-border");
+});
+
+$( ".digitales-options-two" ).click(function() {
+
+  $('.digitales-catigories-two').toggleClass( "shower");
+  $('.digitales-catigories-one').toggleClass( "hider");
+  $('.digitales-options-two').toggleClass( "digitales-b-border");
+  $('.digitales-options-one').toggleClass( "digitales-b-border");
+});
+
+});
---
+++ img/Trimobile_files/mainController.js
@@ -0,0 +1,6 @@
+angular.module('trimobile').controller('mainController', function($scope){
+
+$scope.test = "this app is working"
+
+$scope.isCollapsed = true;
+})
